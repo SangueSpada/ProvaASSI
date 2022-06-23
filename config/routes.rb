bbd65e6b5817aa10786abe980744c999057d975e
SangueSpada <kankilic.1888465@studenti.uniroma1.it>
2022-06-23 15:40:00 +0200

    update

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   root to: 'c_spotify#siginin'
   get '/auth/spotify/callback', to: 'c_spotify#home'
 
-  #resources: post (racchiude get post,patch,put,delete dei post quando verranno implementati)
+  resources :posts #(racchiude get post,patch,put,delete dei post quando verranno implementati)
 end
