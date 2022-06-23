88417fffc1e359185b522714c49f0ec8bfbb3d42
SangueSpada <kankilic.1888465@studenti.uniroma1.it>
2022-06-23 14:14:45 +0200

    update

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   root to: 'c_spotify#siginin'
   get '/auth/spotify/callback', to: 'c_spotify#home'
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
-  # Defines the root path route ("/")
-  # root "articles#index"
+  #resources: post (racchiude get post,patch,put,delete dei post quando verranno implementati)
 end
